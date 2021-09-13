d875fcbf0a1fa34a256dba34f8903662a57fbdbb
Julia Song <julja.song@gmail.com>
2021-09-13 17:02:41 +0900

    20210913_[MOD] SingletonService.java

--- src/test/java/printemps/core/singleton/SingletonTest.java
+++ src/test/java/printemps/core/singleton/SingletonTest.java
@@ -3,6 +3,9 @@ package printemps.core.singleton;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import printemps.core.AppConfig;
 import printemps.core.member.MemberService;
 
@@ -36,5 +39,24 @@ public class SingletonTest {
         Assertions.assertThat(singletonService1).isSameAs(singletonService2);
         // same : 참조값 비교
         // equal 
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        //memberService1 == memberService2
+        Assertions.assertThat(memberService1).isSameAs(memberService2);
+
     }
 }
